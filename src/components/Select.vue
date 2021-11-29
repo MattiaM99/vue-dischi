a7b02d6d1ccabc29feed8d6189ea088f70f4b152
MattiaM99 <mattiamazzoni123@gmail.com>
2021-11-29 16:08:09 +0100

    creato componente select

---
+++ src/components/Select.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="genere">
+    <select
+      v-model="selected"
+      @select="$emit('music', selected)"
+      name="" id="">
+      <option value="rock">Rock</option>
+      <option value="pop">Pop</option>
+      <option value="jazz">Jazz</option>
+      <option value="metal">Metal</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Select",
+  data(){
+    return{
+      music: ""
+    }
+  },
+  methods:{
+  },
+  computed:{
+    select(){
+      return this.music
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.genere{
+  select{
+    padding: 15px 20px;
+    font-weight: bold;
+  }
+}
+</style>
